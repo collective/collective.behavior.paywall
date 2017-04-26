feea6b755c6046ccae8afda6143f45324109f99a
hvelarde <hector.velarde@gmail.com>
2017-04-26 17:54:27 -0300

    Clean up

--- src/collective/behavior/paywall/tests/test_transforms.py
+++ src/collective/behavior/paywall/tests/test_transforms.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 from collective.behavior.paywall.testing import INTEGRATION_TESTING
 from collective.behavior.paywall.transforms import PaywallTransform
-from glob import glob
-from plone import api
-from plone.app.testing import logout
-from repoze.xmliter.utils import getHTMLSerializer
 
 import lxml
-import os
 import unittest
 
 
@@ -15,33 +10,9 @@ class TransformerTestCase(unittest.TestCase):
 
     layer = INTEGRATION_TESTING
 
-    def _get_result(self, html):
-        with open(html, 'r') as f:
-            result = f.read()
-        return getHTMLSerializer(result)
-
-    def _walk_files(self):
-        current_dir = os.path.abspath(os.path.dirname(__file__))
-        html_dir = os.path.join(current_dir, 'html', '*')
-        for html in glob(html_dir):
-            if 'nopaywall' in html:
-                continue
-            result = self._get_result(html)
-            yield result
-
     def setUp(self):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-
-        with api.env.adopt_roles(['Manager']):
-            self.n1 = api.content.create(self.portal, 'News Item', 'n1')
-
-        self.n1.paywall_enabled = True
-        self.request.response.setHeader('Content-Type', 'text/html')
-
-        self.transformer = PaywallTransform(None, self.request)
-        self.transformer.context = self.n1
-        logout()
 
     def test_hide_siblings(self):
         transform = PaywallTransform(None, None)
